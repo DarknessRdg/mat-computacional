8c309802c79746be4d97f5ba04d813dbc2b5d2d2
Luan Rodrigues <luanrodrigues007@hotmail.com>
2021-01-17 01:20:30 -0300

    add numeros primos

---
+++ primos/primo.py
@@ -0,0 +1,15 @@
+def eh_divisivel(numero, divisor):
+    return numero % divisor == 0
+
+
+def eh_primo(numero):
+    intervalo_de_teste = range(2, numero // 2 + 1)
+
+    if numero == 1:
+        return False
+
+    for divisor in intervalo_de_teste:
+        if eh_divisivel(numero, divisor):
+            return False
+
+    return True
---
+++ tests/test_primo.py
@@ -0,0 +1,20 @@
+from primos.primo import eh_primo
+import pytest
+
+
+@pytest.mark.parametrize('primo', [
+    2, 3, 5, 7, 11, 13, 17, 19,
+    23, 29, 31, 37, 41, 43, 47,
+    53, 59, 61, 67, 71, 73, 79,
+    83, 89, 97
+])
+def test_primos(primo):
+    assert eh_primo(primo)
+
+
+@pytest.mark.parametrize('nao_primo', [
+    1, 4, 6, 8, 9, 10, 12, 14, 15, 16,
+    18, 20, 21, 24
+])
+def test_nao_sao_primos(nao_primo):
+    assert not eh_primo(nao_primo)
