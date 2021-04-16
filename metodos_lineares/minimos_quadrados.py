21a768b0c4bf4844f9639571e047124ad2be5f85
Luan Rodrigues <luanrodrigues007@hotmail.com>
2021-04-16 01:18:06 -0300

    min quadrdados linear

---
+++ metodos_lineares/minimos_quadrados.py
@@ -0,0 +1,24 @@
+# function
+# minQuadradosLinear(x, y)
+# {
+#     let[Ex, Ey, Exy, Ex2] = [0, 0, 0, 0];
+#
+# for (let i in x)
+# {
+#     Ex += x[i];
+# Ey += y[i];
+# Exy += x[i] * y[i];
+# Ex2 += x[i] ** 2;
+# }
+#
+# const
+# n = x.length;
+#
+# const
+# a = (n * Exy - Ex * Ey) / (n * Ex2 - Math.pow(Ex, 2));
+#
+# const
+# b = (Ex2 * Ey - Exy * Ex) / (n * Ex2 - Math.pow(Ex, 2));
+#
+# return [a, b]
+# }
